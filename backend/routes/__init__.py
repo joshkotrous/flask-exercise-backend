55483e30f0aa0ac2eaeeeb0a6f5284d98c685cd9
joshkotrous <43544333+joshkotrous@users.noreply.github.com>
2024-03-28 00:24:39 -0400

    added dynamic authorization

--- backend/routes/__init__.py
+++ backend/routes/__init__.py
@@ -1,9 +1,16 @@
 import jwt
 import jwt.algorithms
 from flask_httpauth import HTTPBasicAuth, HTTPTokenAuth
+from sqlalchemy import select
 from werkzeug.security import check_password_hash, generate_password_hash
 
-allowed_users = {"test": generate_password_hash("pass")}
+from backend import db
+from backend.entities.user import User
+
+allowed_users = {
+    "test": generate_password_hash("pass"),
+    "josh": generate_password_hash("Password"),
+}
 
 allowed_tokens = {"token-test": "test"}
 
@@ -15,11 +22,20 @@ secret_token = "mysecret"
 
 @basic_auth.verify_password
 def verify_basic_password(username, password):
-    if username not in allowed_users:
-        return None
+    try:
+        print("username: " + username)
+        user = db.session.scalars(select(User).where(User.email == username)).one()
+        print(user.username)
+        if user is None:
+            return None
 
-    if check_password_hash(allowed_users[username], password):
-        return username
+        if check_password_hash(user.username, password):
+            return username
+
+    except Exception:
+        print(Exception)
+
+    return None
 
 
 @token_auth.verify_token
@@ -28,8 +44,17 @@ def verify_token(token):
         decoded_jwt = jwt.decode(token, secret_token, algorithms=["HS256"])
     except Exception:
         return None
-    if decoded_jwt["name"] in allowed_users:
-        return decoded_jwt["name"]
+    try:
+        user = db.session.scalars(
+            select(User).where(User.username == decoded_jwt["name"])
+        ).one()
+        print(user.username)
+        if user is not None:
+            return decoded_jwt["name"]
+    except Exception:
+        print(Exception)
+    # if decoded_jwt["name"] in allowed_users:
+    #     return decoded_jwt["name"]
     return None
 
     # if token not in allowed_tokens:
